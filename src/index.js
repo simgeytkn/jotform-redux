0135784ff79697113b93c77aab55b0d6f113fc47
simgeytkn <36228273+simgeytkn@users.noreply.github.com>
2019-08-06 14:42:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
 import thunk from 'redux-thunk';
 
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
@@ -13,3 +14,36 @@ ReactDOM.render(
   <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
+
+
+/*
+window.JF.login(
+  function success(){
+    ReactDOM.render(
+      <Provider store={store}><App /></Provider>,
+      document.getElementById('root')
+    );
+    
+  },
+  function error(){
+    alert("Nope")
+  }
+)
+ReactDOM.render(
+  <Provider store={store}><App JF={window.JF} /></Provider>,
+  document.getElementById('root')
+);
+
+window.JF.login(
+  function success(){
+    ReactDOM.render(
+      <Provider store={store}><App /></Provider>,
+      document.getElementById('root')
+    );
+    
+  },
+  function error(){
+    alert("Nope")
+  }
+)
+*/
